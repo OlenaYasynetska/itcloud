51e5ea2dd70c96c55e9c7ad8d85d1189da220296
Olena Ysynetska <olenayasynetska8@gmail.com>
2025-11-12 10:20:08 +0100

    fonds

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import '@fontsource-variable/inter'
-import '@fontsource/poppins/600.css'
-import '@fontsource-variable/space-grotesk'
-import '@fontsource/roboto';
-import '@fontsource/open-sans';
+//import '@fontsource-variable/inter'
+// import '@fontsource/poppins/600.css'
+// import '@fontsource-variable/space-grotesk'
+// import '@fontsource/roboto';
+// import '@fontsource/open-sans';
 
 import './i18n'
 

c7caeb4120cec7bccc85ca4db81a5bd940fe1518
Olena Ysynetska <olenayasynetska8@gmail.com>
2025-11-12 10:17:56 +0100

    fonds

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,9 @@ import './index.css'
 import '@fontsource-variable/inter'
 import '@fontsource/poppins/600.css'
 import '@fontsource-variable/space-grotesk'
+import '@fontsource/roboto';
+import '@fontsource/open-sans';
+
 import './i18n'
 
 createRoot(document.getElementById('root')!).render(
